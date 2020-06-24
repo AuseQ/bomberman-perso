ec6fac751aba1f837ddecb2791532a0194e756f6
Oswald Quevillart <o.quevillart@codeur.online>
2020-06-24 16:37:27 +0200

    optimisation code

--- script.js
+++ script.js
@@ -1,98 +1,64 @@
-
-function random80() {
-return Math.floor(Math.random() * 80);
-}
-
-for (var i = 0; i < random80(); i++) {
-  var divgen = document.createElement("div");
-  divgen.setAttribute("class", "mur");
-  var element = document.getElementById("murs");
-  element.appendChild(divgen);
-var murcss = divgen.style;
-h = divgen.offsetLeft,
-v = divgen.offsetTop;
-h = Math.floor(Math.floor(Math.random()*16)*40);
-v = Math.floor(Math.floor(Math.random()*16)*40);
-murcss.left = String(h) + 'px';
-murcss.top = String(v) + 'px';
-}
-var colliMur = document.querySelectorAll(".mur");
-
-
 var pion = document.getElementById('pion'),
   stylePion = pion.style,
   x = pion.offsetLeft,
   y = pion.offsetTop;
-document.onkeydown = function(event){
-  var event = event || window.event,
-  keyCode = event.keyCode;
-  switch(keyCode){
+
+function random80() {
+  return Math.floor(Math.random() * 80);
+}
+
+for (var i = 0; i < random80() + 20; i++) {
+  var divgen = document.createElement("div");
+  divgen.setAttribute("class", "mur");
+  var element = document.getElementById("murs");
+  element.appendChild(divgen);
+  var murcss = divgen.style,
+    h = divgen.offsetLeft,
+    v = divgen.offsetTop;
+  h = Math.floor(Math.floor(Math.random() * 16) * 40);
+  v = Math.floor(Math.floor(Math.random() * 16) * 40);
+  murcss.left = String(h) + 'px';
+  murcss.top = String(v) + 'px';
+}
+var colliMur = document.querySelectorAll(".mur");
+
+document.onkeydown = function(event) {
+  let keyCode = event.keyCode;
+  switch (keyCode) {
     case 38:
-      y = y - 40;
-      for (var i = 0; i < colliMur.length; i++) {
-        var murGauche = colliMur[i].offsetLeft;
-        var murHaut = colliMur[i].offsetTop;
-        if (x < murGauche + 40 &&
-         x + 40 > murGauche &&
-        y < murHaut + 40 &&
-         40 + y > murHaut) {
-        y = y + 40;
-      }
-      }
-      if (y < 0) {
-        y = 0;
-      }
+      y -= 40;
       break;
     case 39:
-      x = x + 40;
-      for (var i = 0; i < colliMur.length; i++) {
-        var murGauche = colliMur[i].offsetLeft;
-        var murHaut = colliMur[i].offsetTop;
-        if (x < murGauche + 40 &&
-         x + 40 > murGauche &&
-        y < murHaut + 40 &&
-         40 + y > murHaut) {
-        x = x - 40;
-      }
-      }
-      if (x > 640) {
-      x = 640;
-      }
+      x += 40;
       break;
     case 40:
-      y = y + 40;
-
-      for (var i = 0; i < colliMur.length; i++) {
-        var murGauche = colliMur[i].offsetLeft;
-        var murHaut = colliMur[i].offsetTop;
-        if (x < murGauche + 40 &&
-         x + 40 > murGauche &&
-        y < murHaut + 40 &&
-         40 + y > murHaut) {
-        y = y -40;
-      }
-      }
-      if (y > 640) {
-        y = 640;
-      }
+      y += 40;
       break;
     case 37:
-      x = x - 40;
-      for (var i = 0; i < colliMur.length; i++) {
-        var murGauche = colliMur[i].offsetLeft;
-        var murHaut = colliMur[i].offsetTop;
-        if (x < murGauche + 40 &&
-         x + 40 > murGauche &&
-        y < murHaut + 40 &&
-         40 + y > murHaut) {
-        x = x + 40;
-      }
-      }
-      if (x < 0) {
-        x = 0;
-      }
+      x -= 40;
       break;
-}
+    default:
+      return;
+  }
+
+  if (y < 0) y = 0;
+  if (x < 0) x = 0;
+  if (y > 640) y = 640;
+  if (x > 640) x = 640;
+
+  for (var i = 0; i < colliMur.length; i++) {
+    if (x == colliMur[i].offsetLeft && y == colliMur[i].offsetTop) {
+      if (keyCode == 38) {
+        y += 40;
+      } else if (keyCode == 39) {
+        x -= 40;
+      } else if (keyCode == 40) {
+        y -= 40;
+      } else if (keyCode == 37) {
+        x += 40;
+      }
+    }
+  }
 
   stylePion.left = String(x) + 'px';
   stylePion.top = String(y) + 'px';
